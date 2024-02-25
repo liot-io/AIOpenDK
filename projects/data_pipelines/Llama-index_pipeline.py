fd231af82a054771abaf4a4fc031ab427c1948e5
steffen Petersen <steffenadrupbenne@gmail.com>
2024-02-25 21:02:34 +0100

    Update Llama-index_pipeline.py

--- projects/data_pipelines/Llama-index_pipeline.py
+++ projects/data_pipelines/Llama-index_pipeline.py
@@ -1,1 +1,3 @@
 Read me = https://medium.com/@reddyyashu20/llamaindex-create-save-load-indexes-customize-llms-prompts-embeddings-abb581df6dac
+Docs = https://docs.llamaindex.ai/en/stable/getting_started/concepts.html
+
